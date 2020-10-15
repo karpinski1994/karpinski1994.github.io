f014b52014718f0e4512af3d9450e0caf113af05
karpinski1994 <patryk.karpinski@outlook.com>
2020-10-16 00:53:47 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/products-management/public/precache-manifest.29b88c18f8cab5bbca2bbbd5840abd8e.js"
+  "/karpinski1994/products-management/public/precache-manifest.2fea93c9e56ae80c0e44f2c1e04bea2d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/products-management/public/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/karpinski1994/products-management/public/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
